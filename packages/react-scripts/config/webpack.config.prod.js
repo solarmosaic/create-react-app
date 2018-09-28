53ec50354b1b4d3f20d3494c59abbdf734e8ea66
Colin Young <me@colinyoung.com>
2018-09-28 15:34:02 -0500

    Bring back graphql/loader

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -425,6 +425,10 @@ module.exports = {
               'sass-loader'
             ),
           },
+          {
+            test: /\.(graphql)$/,
+            loader: 'graphql-tag/loader',
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
